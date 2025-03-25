1c83afda87fd25985da32e602ed2aa6566f5f368
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2025-03-25 14:50:49 +0100

    move sleep log from f64 to lines

--- src/log.rs
+++ src/log.rs
@@ -2,13 +2,24 @@ use std::fs::File;
 use std::process::exit;
 use std::io::{self,BufReader, BufRead};
 
+use ratatui::text::Line;
+
+#[derive(Clone)]
 pub struct LogController {
-    pub hours_slept: Vec::<f64>,
+    pub hours_slept: Vec::<Line<'static>>,
     pub line_amount: i32
 }
 
 //TODO: move from txt to csv file
 impl LogController {
+
+    pub fn new() -> Self {
+        Self {
+            hours_slept: Vec::new(),
+            line_amount: 0,
+        }
+    }
+    
     pub fn read_file(&mut self, data_path: &String) -> io::Result<()> {
         println!("Reading the contents of {data_path}...");
         let data_file = match File::open(data_path) {
@@ -22,9 +33,9 @@ impl LogController {
         let read_buffer = BufReader::new(data_file);
 
         //parses the string to a float and puts it in the log struct
-        for line in read_buffer.lines() {
-            let line = line?;
-            self.hours_slept.push(line.parse::<f64>().ok().expect("ERROR: data is not formatted correctly"));
+        for entry in read_buffer.lines() {
+            let entry = entry?;
+            self.hours_slept.push(Line::from(entry));
             //keeps track of number of entries
             self.line_amount += 1;
         }
